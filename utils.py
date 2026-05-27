92aa6903101eedfad58a3de1c577238bc95938be
lordkohan <lordkohan@gmail.com>
2026-05-26 23:37:25 -0500

    Update utils.py
    Fix pytgcalls import error

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ try:
     import os
     import subprocess
     import sys
-    from pytgcalls.exceptions import GroupCallNotFoundError
+    from pytgcalls.exceptions import GroupCallNotFound
     from config import Config
     import ffmpeg
     from pyrogram import emoji
